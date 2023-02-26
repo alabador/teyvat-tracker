bf1b52c29d2885fe715218ecec7a3a9f58079bb9
alabador <austinlabador@gmail.com>
2023-02-26 03:28:20 -0800

    Update carousel for responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,23 @@
 
 function Hero() {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full max-h-[65vh] bg-slate-50">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src="/windblumes-breath.webp" className="w-full" />
+                <img src="/windblumes-breath.webp" className="w-full object-cover object-top 2xl:object-contain" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide3" className="btn btn-circle">❮</a> 
                 <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div> 
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="/dev-discussion.webp" className="w-full" />
+                <img src="/dev-discussion.webp" className="w-full object-cover object-top 2xl:object-contain" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide1" className="btn btn-circle">❮</a> 
                 <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div> 
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="/liyue-cuisine.webp" className="w-full" />
+                <img src="/liyue-cuisine.webp" className="w-full object-cover object-top 2xl:object-contain" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide2" className="btn btn-circle">❮</a> 
                 <a href="#slide1" className="btn btn-circle">❯</a>
