e23e39a2a222f728d3c1d2d0e124891a1167cb1a
alabador <austinlabador@gmail.com>
2023-02-26 03:10:34 -0800

    Add header and news carousel

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,28 @@
 
 function Hero() {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/03/Genshin-Impact-Inazuma-Island.jpg")` }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Welcome to Teyvat</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
-                 </div>
-            </div>
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src="/windblumes-breath.webp" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide3" className="btn btn-circle">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src="/dev-discussion.webp" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" className="btn btn-circle">❮</a> 
+                <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src="/liyue-cuisine.webp" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide2" className="btn btn-circle">❮</a> 
+                <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
+import Navlinks from "./Navlinks";
+
 
 function Navbar({name}) {
     return(
         <div className="navbar justify-between">
-            <button className="btn btn-ghost normal-case text-xl" href="#">{name}</button>
-            
+            <button className="btn btn-ghost normal-case text-xl" href="#"><img src="/mond-icon.png" alt="mondstadt icon" className="w-12"></img>{name}</button>
+            <Navlinks />
         </div>
     )
 }
