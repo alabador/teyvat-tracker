04e09e0fb6e94a451648486dc46698e95550a64f
alabador <austinlabador@gmail.com>
2023-02-26 20:19:11 -0800

    Convert data to json

--- src/data/data.py
+++ src/data/data.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+import json
 
 
 url = "https://genshin-impact.fandom.com/wiki/Event"
@@ -23,13 +24,33 @@ current_table_body = current_table.find('tbody')
 rows = current_table_body.find_all('tr')
 
 for row in rows: 
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
     image_td = row.find('img')
     if image_td != None:
         if image_td.has_attr('data-src'):
             image_src = image_td['data-src']
         else: 
             image_src = image_td['src']
-        my_data.append(image_src)
+        my_data.append([ele for ele in cols if ele] + [image_src])
 
-print(my_data)
+# print(my_data)
+
+formatted_data = []
+
+def create_dict(data):
+    for item in data:
+        data_object = {}
+        data_object['title'] = item[0]
+        data_object['dates'] = item[1]
+        data_object['category'] = item[2]
+        data_object['image'] = item[3]
+        formatted_data.append(data_object)
+
+create_dict(my_data)
+
+json_data = json.dumps(formatted_data, indent=2)
+
+print(json_data)
+        
 
