5762aaa21690ed322d43ef9c6a157da5688e69b6
alabador <austinlabador@gmail.com>
2023-02-26 19:42:19 -0800

    Get image src urls from all current events

--- src/data/data.py
+++ src/data/data.py
@@ -10,4 +10,26 @@ data = requests.get(url)
 my_data = []
 
 html = BeautifulSoup(data.text, 'html.parser')
-current = html.find(id="Current-collapsible-section")
+
+# get all tables and classes
+# print('Classes of each table:')
+# for table in html.find_all('table'):
+#     print(table.get('class'))
+
+tables = html.find_all('table')
+current_table = html.find('table', class_='wikitable sortable')
+current_table_body = current_table.find('tbody')
+
+rows = current_table_body.find_all('tr')
+
+for row in rows: 
+    image_td = row.find('img')
+    if image_td != None:
+        if image_td.has_attr('data-src'):
+            image_src = image_td['data-src']
+        else: 
+            image_src = image_td['src']
+        my_data.append(image_src)
+
+print(my_data)
+
