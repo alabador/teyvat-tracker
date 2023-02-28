6f8db72eb5ce6d9becbc02349642245c8ca9b3c2
alabador <austinlabador@gmail.com>
2023-02-27 18:36:33 -0800

    Update layout and color themes

--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -4,7 +4,7 @@ import AnnouncementCard from './AnnouncementCard';
 
 const Announcements = () => {
     return(
-        <div className="grid col-span-3 bg-white p-6">
+        <div className="flex flex-col col-span-3 bg-white p-6">
             <section className='flex flex-col gap-6'>
                 {EventData.map(gameEvent => {
                     const updatedImage = gameEvent.image.split('/revision')[0]
@@ -16,6 +16,10 @@ const Announcements = () => {
                 }         
                 )}
             </section>
+            <div className='flex flex-col justify-center flex-grow items-center bg-gray-100 mt-6'>
+                <img src='/paimon-sleep.png' alt="paimon sleeping" className='w-16'></img>
+                <p className='text-sm pt-4'>No more current events. Check back soon for updates!</p>
+            </div>
         </div>
     )
 }
