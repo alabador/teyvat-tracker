7dffdfe12e3f37acfa746e26abc8ec66ecf2fdc2
alabador <austinlabador@gmail.com>
2023-03-06 14:30:04 -0800

    Add rarity bg to cards, change layout slightly

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const Character = ({charName}) => {
     const [charInfo, setCharInfo] = useState('');
+    const [element, setElement] = useState('');
     useEffect(() => {
         const url = `https://api.genshin.dev/characters/${charName}`;
 
@@ -19,15 +20,30 @@ const Character = ({charName}) => {
         fetchData();
     }, [charName]);
 
+    // useEffect(() => {
+    //     const url = `https://api.genshin.dev/elements/${charInfo.vision.toLowerCase()}`
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(url);
+    //             const json = await response.json();
+    //             setElement(json);
+    //         }
+    //         catch (error) {
+    //             console.log("error", error)
+    //         }
+    //     }
+    // })
+
 
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={`https://api.genshin.dev/characters/${charName}/icon-big`} alt="character card" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{charName}</h2>
+        <div className="card items-center justify-evenly bg-base-100 shadow-xl">
+            <figure className={charInfo.rarity === 5 ? 'bg-yellow-500 bg-opacity-80 h-3/5': 'bg-violet-800 bg-opacity-70 h-3/5'}><img src={`https://api.genshin.dev/characters/${charName}/icon-big`} alt="character card" /></figure>
+            <div className="py-4">
+                <h2 className="card-title capitalize">{charName}</h2>
             </div>
-            <div className="card-actions justify-center">
+            <div className="flex justify-center">
                 <div className="inline-block px-2 py-1 text-sm font-semibold rounded-md bg-blue-400 text-gray-900">{charInfo.vision}</div>
+                {/* <img src={`https://api.genshin.dev/elements/${charInfo.vision.toLowerCase()}/icon`} alt="element icon"></img> */}
             </div>
         </div>
     )
