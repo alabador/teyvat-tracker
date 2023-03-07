8a1ef4276379d7c93fd5b90b9713ab450c4c75d8
alabador <austinlabador@gmail.com>
2023-03-06 21:06:36 -0800

    Update announcement card and font

--- src/components/AnnouncementCard.js
+++ src/components/AnnouncementCard.js
@@ -1,9 +1,9 @@
 
 const AnnouncementCard = ({imgSrc, dates, title,}) => {
     return (
-        <article className="grid grid-cols-4 bg-red-50 dark:bg-orange-200 p-4 rounded-xl">
+        <article className="grid grid-cols-4 bg-red-50 dark:bg-orange-50 p-4 rounded-xl">
             <div className="col-span-3">
-                <h1 className="font-bold lg:text-xl text-md pb-4">{title}</h1>
+                <h1 className="font-bold lg:text-2xl text-md pb-4">{title}</h1>
                 <p className="font-extralight text-xs">{dates}</p>
             </div>
             <img src={imgSrc} alt="event" className="rounded-xl object-cover w-full col-start-4 col-span-1"></img>
