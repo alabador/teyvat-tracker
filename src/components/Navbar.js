63eb28bddc9d2ac972d399f1a629e13eb71fb926
alabador <austinlabador@gmail.com>
2023-03-06 20:50:37 -0800

    Change bg to white for hero news and nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Navlinks from "./Navlinks";
 
 function Navbar({name}) {
     return(
-        <div className="navbar justify-between">
+        <div className="navbar justify-between bg-white">
             <button className="btn btn-ghost normal-case text-xl" href="#"><img src="/mond-icon.png" alt="mondstadt icon" className="w-12"></img>{name}</button>
             <Navlinks />
         </div>
